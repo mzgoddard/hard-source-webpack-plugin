8448e1ce249a3edf7b5c54338b6d6f05b9fc80f8
Michael "Z" Goddard <mzgoddard@gmail.com>
2016-12-09 14:42:19 -0500

    Test loaders that have no dependencies
    A NormalModule can have no dependencies if it has a pitch loader
    execute on it and not record any file or context dependencies. Test
    that that no-dep module can be depended on without rebuilds and that
    the module itself can use hard modules.

---
+++ tests/fixtures/loader-custom-no-dep-moved/loader.js
@@ -0,0 +1,9 @@
+var fs = require('fs');
+var path = require('path');
+
+module.exports = function(source) {};
+
+module.exports.pitch = function(remainingRequest) {
+  this.cacheable && this.cacheable();
+  return '// ' + remainingRequest;
+};
