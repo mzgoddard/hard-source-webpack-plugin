aa112a4ee9a7fc32175bf89d0982dfc8d3737820
Michael "Z" Goddard <mzgoddard@gmail.com>
2017-12-10 20:34:20 -0500

    Remove unnecessary level require from index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ var crypto = require('crypto');
 var fs = require('fs');
 var path = require('path');
 
-var level = require('level');
 var lodash = require('lodash');
 var mkdirp = require('mkdirp');
 
