7e072918cd4c6c8ce088faf0aec528070b9b61d0
Michael "Z" Goddard <mzgoddard@gmail.com>
2018-01-22 19:31:41 -0500

    Add test fixtures use pathinfo and records features

---
+++ tests/fixtures/base-path-info/webpack.config.js
@@ -0,0 +1,19 @@
+var HardSourceWebpackPlugin = require('../../..');
+
+module.exports = {
+  context: __dirname,
+  entry: './index.js',
+  output: {
+    path: __dirname + '/tmp',
+    filename: 'main.js',
+    pathinfo: true,
+  },
+  plugins: [
+    new HardSourceWebpackPlugin({
+      cacheDirectory: 'cache',
+      environmentHash: {
+        root: __dirname + '/../../..',
+      },
+    }),
+  ],
+};
