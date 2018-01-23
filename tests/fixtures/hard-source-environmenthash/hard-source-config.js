20d237ee5e8768b11a9b346fdd13c24e7c804eef
Michael "Z" Goddard <mzgoddard@gmail.com>
2018-01-22 19:32:20 -0500

    chore(tests): commit last test states

--- tests/fixtures/hard-source-environmenthash/hard-source-config.js
+++ tests/fixtures/hard-source-environmenthash/hard-source-config.js
@@ -1,6 +1,5 @@
 {
   cacheDirectory: "cache",
-  recordsPath: "cache/records.json",
   environmentHash: function(config) {
     return new Promise(function(resolve, reject) {
       fs.readFile(__dirname + "/env-hash", "utf8", function(err, src) {
