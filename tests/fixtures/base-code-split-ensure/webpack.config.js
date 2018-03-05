928afd7c0c754750d965188193f7d3186f2db1fb
Michael "Z" Goddard <mzgoddard@gmail.com>
2018-03-05 11:38:56 -0500

    fix: Use more generalized cycle detection on dependency block member (#285)
    * Test require.ensure
    
    * Use more generalized cycle detection on dependency block member
    
    Break up dependency -> block -> dependency cycles with a generic test instead
    of testing if the dependency is one of a set of specific types.

---
+++ tests/fixtures/base-code-split-ensure/webpack.config.js
@@ -0,0 +1,15 @@
+var HardSourceWebpackPlugin = require('../../..');
+
+module.exports = {
+  context: __dirname,
+  entry: './index.js',
+  output: {
+    path: __dirname + '/tmp',
+    filename: 'main.js',
+  },
+  plugins: [
+    new HardSourceWebpackPlugin({
+      cacheDirectory: 'cache',
+    }),
+  ],
+};
