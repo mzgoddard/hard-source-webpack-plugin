2e914621d9d644d62a5e16c5d792fedd0163b033
Michael "Z" Goddard <mzgoddard@gmail.com>
2018-05-12 10:23:28 -0400

    Test MiniCss support based on test/util/features

--- tests/plugins-webpack-4.js
+++ tests/plugins-webpack-4.js
@@ -4,10 +4,12 @@ var describeWP = require('./util').describeWP;
 var itCompilesTwice = require('./util').itCompilesTwice;
 var itCompilesHardModules = require('./util').itCompilesHardModules;
 
+var c = require('./util/features');
+
 describeWP(4)('plugin webpack 4 use', function() {
 
-  itCompilesTwice('plugin-mini-css-extract');
-  itCompilesTwice('plugin-mini-css-extract', {exportStats: true});
-  itCompilesHardModules('plugin-mini-css-extract', ['./index.css']);
+  itCompilesTwice.skipIf([c.miniCss])('plugin-mini-css-extract');
+  itCompilesTwice.skipIf([c.miniCss])('plugin-mini-css-extract', {exportStats: true});
+  itCompilesHardModules.skipIf([c.miniCss])('plugin-mini-css-extract', ['./index.css']);
 
 });
--- tests/util/features.js
+++ tests/util/features.js
@@ -1,10 +1,13 @@
 const webpackVersion = require('webpack/package.json').version;
 
 const isWebpackBeta = /alpha|beta|rc/.test(webpackVersion);
-const webpack3OrEarlier = Number(webpackVersion.split('.')[0]) < 4;
+const webpackMajor = Number(webpackVersion.split('.')[0]);
+const webpack3OrEarlier = webpackMajor < 4;
+const webpack4OrLater = webpackMajor >= 4;
 
 module.exports = {
   extractText: !isWebpackBeta && webpack3OrEarlier,
+  miniCss: webpack4OrLater,
   uglify: !isWebpackBeta,
-  html: !isWebpackBeta && webpack3OrEarlier,
+  html: !isWebpackBeta,
 };
