4d0b45f6406138e786a4fe8ea6dfe9dde975f09c
Michael "Z" Goddard <mzgoddard@gmail.com>
2018-05-17 02:12:58 -0400

    Test Append2Serializer

--- tests/serializers.js
+++ tests/serializers.js
@@ -18,6 +18,7 @@ function promisify(f, o) {
 }
 
 var AppendSerializerPlugin = require('../lib/hard-source-append-serializer-plugin');
+var Append2SerializerPlugin = require('../lib/hard-source-append-2-serializer-plugin');
 
 var itCompilesTwice = require('./util').itCompilesTwice;
 var itCompilesChange = require('./util').itCompilesChange;
@@ -25,6 +26,7 @@ var itCompilesChange = require('./util').itCompilesChange;
 describe('hard source serializers - compiles identically', function() {
 
   itCompilesTwice('serializer-append-base-1dep');
+  itCompilesTwice('serializer-append-2-base-1dep');
   itCompilesTwice('serializer-cacache-base-1dep');
   itCompilesTwice('serializer-json-base-1dep');
   itCompilesTwice('serializer-leveldb-base-1dep');
@@ -75,6 +77,62 @@ describe('hard source serializers - serializer abilities', function() {
     })
     .then(function(_stat) {
       expect(oldSize).to.be.gt(_stat.size);
+    });
+  });
+
+  itCompilesChange('serializer-append-2-base-1dep-compact', {
+    'fib.js': [
+      'module.exports = function(n) {',
+      '  return n + (n > 0 ? n - 1 : 0);',
+      '};',
+    ].join('\n'),
+    'fib/index.js': null,
+  }, {
+    'fib.js': null,
+    'fib/index.js': [
+      'module.exports = function(n) {',
+      '  return n + (n > 0 ? n - 2 : 0);',
+      '};',
+    ].join('\n'),
+  }, function(output) {
+    expect(output.run1['main.js'].toString()).to.match(/n - 1/);
+    expect(output.run2['main.js'].toString()).to.match(/n - 2/);
+
+    var cachePath = join(__dirname, 'fixtures/serializer-append-2-base-1dep-compact/tmp/cache');
+
+    var stat = promisify(fs.stat);
+    var oldSize;
+
+    return Promise.resolve()
+    .then(function() {
+      return Append2SerializerPlugin.createSerializer({
+        name: 'md5',
+        autoParse: true,
+        cacheDirPath: cachePath
+      })
+      .sizes();
+    })
+    .then(function(_stat) {
+      oldSize = _stat.total;
+    })
+    .then(function() {
+      return Append2SerializerPlugin.createSerializer({
+        name: 'md5',
+        autoParse: true,
+        cacheDirPath: cachePath
+      })
+      .compact();
+    })
+    .then(function() {
+      return Append2SerializerPlugin.createSerializer({
+        name: 'md5',
+        autoParse: true,
+        cacheDirPath: cachePath
+      })
+      .sizes();
+    })
+    .then(function(_stat) {
+      expect(oldSize).to.be.gt(_stat.total);
     });
   });
 
