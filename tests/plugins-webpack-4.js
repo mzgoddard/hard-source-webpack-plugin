7ee94966fd9eb8221d323b4413804acd3876d7b4
Michael "Z" Goddard <mzgoddard@gmail.com>
2018-05-25 11:29:37 -0400

    Test changes to css with mini-css-extract-plugin

--- tests/plugins-webpack-4.js
+++ tests/plugins-webpack-4.js
@@ -3,6 +3,7 @@ var expect = require('chai').expect;
 var describeWP = require('./util').describeWP;
 var itCompilesTwice = require('./util').itCompilesTwice;
 var itCompilesHardModules = require('./util').itCompilesHardModules;
+var itCompilesChange = require('./util').itCompilesChange;
 
 var c = require('./util/features');
 
@@ -11,5 +12,26 @@ describeWP(4)('plugin webpack 4 use', function() {
   itCompilesTwice.skipIf([c.miniCss])('plugin-mini-css-extract');
   itCompilesTwice.skipIf([c.miniCss])('plugin-mini-css-extract', {exportStats: true});
   itCompilesHardModules.skipIf([c.miniCss])('plugin-mini-css-extract', ['./index.css']);
+
+});
+
+describeWP(4)('plugin webpack 4 use - builds change', function() {
+
+  itCompilesChange('plugin-mini-css-extract-change', {
+    'index.css': [
+      '.hello {',
+      '  color: blue;',
+      '}',
+    ].join('\n'),
+  }, {
+    'index.css': [
+      '.hello {',
+      '  color: red;',
+      '}',
+    ].join('\n'),
+  }, function(output) {
+    expect(output.run1['main.css'].toString()).to.match(/blue/);
+    expect(output.run2['main.css'].toString()).to.match(/red/);
+  });
 
 });
